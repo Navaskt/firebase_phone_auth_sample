947867bcabb38e9b16c3a9d14b68b5b17f573b1b
Mohammed Navas <navaskt38@gmail.com>
2025-11-13 11:32:58 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebase_phone_auth_sample
+Flutter firebase phone authentication sample project
---
+++ android/app/src/main/kotlin/com/example/firebase_phone_auth_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_phone_auth_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
